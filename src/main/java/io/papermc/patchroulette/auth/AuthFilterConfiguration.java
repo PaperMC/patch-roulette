377adef45d0acea73078342201885199a4c1b772
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-01-25 17:35:45 -0700

    fix posts
    thanks riley

--- src/main/java/io/papermc/patchroulette/auth/AuthFilterConfiguration.java
+++ src/main/java/io/papermc/patchroulette/auth/AuthFilterConfiguration.java
@@ -3,11 +3,18 @@ package io.papermc.patchroulette.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class AuthFilterConfiguration {
 
     private final AuthFilter authFilter;
@@ -19,10 +26,15 @@ public class AuthFilterConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> {
-            authorizeRequests.anyRequest().authenticated();
-        });
         http.addFilterBefore(this.authFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+        return http
+            .authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+            .cors(Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(Customizer.withDefaults())
+            .logout(AbstractHttpConfigurer::disable)
+            .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
     }
 }
