11cbcc3b40c7d610113819ae682a87a5a417f46e
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-03-12 19:36:19 -0700

    Use cf adapter in prod

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from "svelte-adapter-bun";
+import bunAdapter from "svelte-adapter-bun";
+import cloudflareAdapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +14,7 @@ const config = {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter(),
+        adapter: dev ? bunAdapter() : cloudflareAdapter(),
     },
 };
 
