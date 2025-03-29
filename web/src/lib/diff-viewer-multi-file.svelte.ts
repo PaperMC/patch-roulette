51df284bad9b9b796b3a38a5ea661f20141dbc81
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-03-28 19:21:34 -0700

    suppress warn

--- web/src/lib/diff-viewer-multi-file.svelte.ts
+++ web/src/lib/diff-viewer-multi-file.svelte.ts
@@ -30,6 +30,7 @@ export class GlobalOptions {
             };
         });
 
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
         const opts = this;
         onMount(() => {
             function storageChanged(event: StorageEvent) {
