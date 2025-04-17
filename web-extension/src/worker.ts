730b25d047df8692248e530c60030b7071c58446
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-04-16 21:19:25 -0700

    Use webextension-polyfill to fix context menu action in firefox

--- web-extension/src/worker.ts
+++ web-extension/src/worker.ts
@@ -1,13 +1,14 @@
 import { openInPatchRoulette } from "./common.ts";
+import browser from "webextension-polyfill";
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
+browser.runtime.onInstalled.addListener(() => {
+    browser.contextMenus.create({
         id: "open-in-patch-roulette",
         title: "Open in Patch Roulette Diff Viewer",
         contexts: ["all"],
     });
 
-    chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+    browser.contextMenus.onClicked.addListener(async (info, tab) => {
         let url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl || info.pageUrl;
         if (!url && tab && tab.url) {
             url = tab.url;
