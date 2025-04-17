1c717d516af571d5bff61b6cb84f948ee4de512c
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-04-16 21:34:04 -0700

    Fix context menu item persistence

--- web-extension/src/worker.ts
+++ web-extension/src/worker.ts
@@ -1,20 +1,23 @@
 import { openInPatchRoulette } from "./common.ts";
 import browser from "webextension-polyfill";
 
-browser.runtime.onInstalled.addListener(() => {
+function createContextMenu() {
     browser.contextMenus.create({
         id: "open-in-patch-roulette",
         title: "Open in Patch Roulette Diff Viewer",
         contexts: ["all"],
     });
+}
 
-    browser.contextMenus.onClicked.addListener(async (info, tab) => {
-        let url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl || info.pageUrl;
-        if (!url && tab && tab.url) {
-            url = tab.url;
-        }
-        if (url) {
-            await openInPatchRoulette(url);
-        }
-    });
+browser.runtime.onStartup.addListener(createContextMenu);
+browser.runtime.onInstalled.addListener(createContextMenu);
+
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+    let url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl || info.pageUrl;
+    if (!url && tab && tab.url) {
+        url = tab.url;
+    }
+    if (url) {
+        await openInPatchRoulette(url);
+    }
 });
