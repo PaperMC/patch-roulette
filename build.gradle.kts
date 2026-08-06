29ff7e2c3bb35e878e44bc88e6035eba53ff1f03
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2026-08-06 09:44:58 -0700

    Update plugin org.springframework.boot to v4.1.0
    Also renames spring-boot-starter-web to spring-boot-starter-webmvc, removes the explicit H2 version to use the managed version, and removes the redundant management.endpoints.enabled-by-default property.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.time.Instant
 plugins {
     id("net.kyori.indra") version "4.0.0"
     id("net.kyori.indra.git") version "4.0.0"
-    id("org.springframework.boot") version "3.5.16"
+    id("org.springframework.boot") version "4.1.0"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.google.cloud.tools.jib") version "3.5.4"
 }
@@ -17,13 +17,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-loader")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    runtimeOnly("com.h2database:h2:2.4.240") // for local
+    runtimeOnly("com.h2database:h2") // for local
     runtimeOnly("org.postgresql:postgresql") // for prod
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
